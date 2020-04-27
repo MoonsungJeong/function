a6345e01c61885d7d809a021e59fd46394551a1f
Moonsung <gmsj159@gmail.com>
2020-04-27 14:09:25 +1000

    Ajax Polling & chat system version2(fixed CORS policy)

--- Ajax_Polling/server.js
+++ Ajax_Polling/server.js
@@ -4,6 +4,11 @@ var url = require('url');
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Database array
 var DB = [];
